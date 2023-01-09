4adbc75fca3ea04162feef6f7167a9731cd69b6d
Pushpendra Singh Thakur <pushpendrasinghthakur002@gmail.com>
2023-01-10 03:41:31 +0530

    commit

--- App.js
+++ App.js
@@ -1,98 +1,71 @@
-const taskInput = document.querySelector('.taskInput');
 const submit = document.querySelector('.submit')
-const list = document.querySelector('.list');
+
 const deletebtn = document.querySelector('.remove');
-const magicboxes = document.querySelectorAll('.one')
-console.log(magicboxes);
-submit.addEventListener('click', addTask);
 
-deletebtn.addEventListener('click', deletetasks);
-
-list.addEventListener('dragstart', () => {
-    console.log('dragstart has been triggered')
-})
-list.addEventListener('dragend', () => {
-    console.log('dragend has been triggered')
-})
-
-for (magicBox of magicboxes) {
-    magicBox.addEventListener('dragover', (e) => {
-        console.log('dragover has been triggered');
-        e.preventDefault();
-    })
-    magicBox.addEventListener('dragenter', () => {
-        console.log('dragenter has been triggered')
-    })
-    magicBox.addEventListener('dragleave', () => {
-        console.log('dragleave has been triggered')
-    })
-    magicBox.addEventListener('drop', () => {
-        console.log('drop has been triggered')
-    })
+function allowDrop(e) {
+    e.preventDefault();
 }
 
+function drag(e) {
+    e.dataTransfer.setData("text", e.target.id);
+}
 
-function deletetasks(e) {
-    let tasks = [];
-    if (confirm('Delete all tasks')) {
-        localStorage.setItem('tasks', JSON.stringify('tasks'));
-        location.reload();
+function drop(e) {
+    e.preventDefault();
+    var data = e.dataTransfer.getData("text");
+    if (e.target.id === "card-contents") {
+        e.target.appendChild(document.getElementById(data));
     }
 
 }
+
+function deleteTask(event) {
+    let del = event.currentTarget.parentNode.parentNode;
+    del.removeChild(event.currentTarget.parentNode);
+}
+submit.addEventListener('click', addTask);
+let count = 1;
 
 function addTask(e) {
     e.preventDefault();
-    if (taskInput.value) {
-        console.log('true')
-        const li = document.createElement('li');
-        li.className = 'task-item';
-        li.style.border = "1px solid black"
-        li.style.margin = '10px';
-        li.style.padding = '8px'
-        li.draggable = 'true';
-        li.textContent = taskInput.value;
-        console.log(document.querySelector('.taskInput').value);
-        list.appendChild(li);
-        addTaskToLocalStorage(li.textContent);
-        taskInput.value = '';
+    var list = document.querySelector('.list');
+    let taskDesc = document.querySelector("#taskdescription");
+    let taskname = document.querySelector('.taskInput');
+
+    if (taskname.value === "") {
+        alert('enter a task')
+        return;
     }
-    else {
-        alert('Please Enter task')
-    }
+
+    let taskDiv = document.createElement("div");
+    taskDiv.classList.add("task");
+    taskDiv.setAttribute("id", count);
+    taskDiv.draggable = "true";
+    taskDiv.ondragstart = drag;
+
+    let taskHead = document.createElement("div");
+    taskHead.classList.add("task-heading");
+
+    taskHead.textContent = count + ".     " + taskname.value;
+    taskname.value = "";
+    count++;
+
+    let taskBody = document.createElement("div");
+    taskBody.classList.add("task-body");
+
+    taskBody.textContent = taskDesc.value;
+    taskDesc.value = "";
+
+    let delbtn = document.createElement("button")
+    delbtn.innerHTML = "Delete"
+    delbtn.onclick = deleteTask;
+    delbtn.classList.add("deletebutton");
+
+    taskDiv.appendChild(taskHead);
+    taskDiv.appendChild(taskBody);
+    taskDiv.appendChild(delbtn)
+
+    list.appendChild(taskDiv);
 
 
 }
-
-function addTaskToLocalStorage(task) {
-    let tasks;
-    if (localStorage.getItem('tasks') === null) {
-        tasks = [];
-    }
-    else {
-        tasks = JSON.parse(localStorage.getItem('tasks'));
-    }
-    tasks.push(task);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-}
-function getTasks() {
-    let task;
-    if (localStorage.getItem('tasks') === null) {
-        tasks = [];
-    }
-    else {
-        tasks = JSON.parse(localStorage.getItem('tasks'));
-    }
-    tasks.forEach(task => {
-
-        const li = document.createElement('li');
-
-        li.className = 'task-item';
-        li.textContent = task;
-
-        list.appendChild(li);
-    });
-
-
-}
-getTasks();
